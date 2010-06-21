1ff163eed55e440a253f11d2fbd7d426626c12d5
Roman Zaharov <zahardzhan@gmail.com>
2010-06-21 23:09:33 +1100

    Add and remove hooks for vars and functions.

--- README.md
+++ README.md
@@ -1,50 +1,65 @@
-# Robert Hooke
+# Hooks
 
-Robert Hooke provides a flexible, composable mechanism by which you
-can extend behaviour of functions after they've been defined. It's named
-after [Robert Hooke FRS](http://en.wikipedia.org/wiki/Robert_Hooke), a
-founding member of the Royal Society who made many important
-discoveries in the fields of Gravitation, Microscopy, and Astronomy.
+Hooks provides a flexible, composable mechanism by which you
+can extend behaviour of functions after they've been defined.
 
 Add this to your project.clj :dependencies list:
 
-    [robert/hooke "1.0.0"]
+    [hooks "1.0.0"]
 
-If you would like to make your software extensible using Hooke, all
+If you would like to make your software extensible using Hooks, all
 you need to do is provide a convention for namespaces that will get
 loaded on startup. Then users can place files that call add-hook under
 a specific namespace prefix (my.program.hooks.*) which they can rely
 on getting loaded at startup.
 
-Hooke is inspired by Emacs Lisp's defadvice and clojure.test fixtures.
+Hooks is inspired by Emacs Lisp's defadvice, clojure.test fixtures and
+CLOS multimethods.
 
 ## Usage
 
-    (use 'robert.hooke)
+    (use 'hooks)
 
     (defn examine [x]
-      (println x))
+      (print x))
 
-    (defn microscope
-      "The keen powers of observation enabled by Robert Hooke allow
-      for a closer look at any object!"
-      [f x]
+    (defn microscope [f x]             ;; x -> X
       (f (.toUpperCase x)))
 
-    (defn doubler [f & args]
-      (apply f args)
-      (apply f args))
+    (defn doubler [f & xs]             ;; x -> xx
+      (apply f xs)
+      (apply f xs))
 
-    (defn telescope [f x]
-      (f (apply str (interpose " " x))))
+    (defn telescope [f x]              ;; xx -> x x
+     (f (apply str (interpose " " x))))
 
-    (add-hook #'examine microscope)
-    (add-hook #'examine doubler)
-    (add-hook #'examine telescope)
+    (defn into-string [f & xs]
+      (with-out-str (apply f xs)))
 
-    (examine "something")
-    > S O M E T H I N G
-    > S O M E T H I N G
+    (add-hook :around #'examine :microscope microscope)
+    (add-hook :around #'examine :doubler    doubler)
+    (add-hook :around #'examine :telescope  telescope)
+    (add-hook :around #'examine :into-str   into-string)
+    (add-hook :after  #'examine :dotspace   (fn [& args] (print \. \space)))
+    
+    (examine "Before i forget")
+    > B E F O R E   I   F O R G E T.  B E F O R E   I   F O R G E T.  
+    
+    (remove-hook :after  #'examine :dotspace)
+    (remove-hook :around #'examine :doubler)
+    
+    (examine "Before i forget")
+    > B E F O R E   I   F O R G E T
+    
+    (remove-hook :around #'examine :microscope)
+    
+    (examine "Before i forget")
+    > B e f o r e   i   f o r g e t
+    
+    (remove-hook :around #'examine :telescope)
+    
+    (examine "Before i forget")
+    > Before i forget
 
 Hooks are functions that wrap other functions. They receive the
 original function and its arguments as their arguments. Hook
@@ -60,6 +75,6 @@ target function.
 
 ## License
 
-Copyright (C) 2010 Phil Hagelberg and Kevin Downey
+Copyright (C) 2010 Phil Hagelberg, Kevin Downey and Roman Zaharov
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/hooks.clj
+++ src/hooks.clj
@@ -1,32 +1,12 @@
-(ns robert.hooke
-  "Hooke your functions!
+(ns hooks)
 
-    (defn examine [x]
-      (println x))
+(defmacro with-return [val & body]
+  `(let [return# (do ~val)]
+     (do ~@body)
+     return#))
 
-    (defn microscope
-      \"The keen powers of observation enabled by Robert Hooke allow
-      for a closer look at any object!\"
-      [f x]
-      (f (.toUpperCase x)))
-
-    (defn doubler [f & args]
-      (apply f args)
-      (apply f args))
-
-    (defn telescope [f x]
-      (f (apply str (interpose \" \" x))))
-
-    (add-hook #'examine microscope)
-    (add-hook #'examine doubler)
-    (add-hook #'examine telescope)
-
-    ;; Now when we examine something:
-    (examine \"something\")
-    > SOMETHING
-    > SOMETHING
-
-  Use the add-hook function to wrap a function in your a hook.")
+(defn- multifn? [x]
+  (instance? clojure.lang.MultiFn x))
 
 (defn- compose-hooks [f1 f2]
   (fn [& args]
@@ -38,29 +18,76 @@
 (defn- run-hooks [hooked original args]
   (apply (join-hooks original @(::hooks (meta hooked))) args))
 
-(defn- prepare-for-hooks [v]
-  (when-not (::hooks (meta @v))
-    (alter-var-root v (fn [original]
-                        (with-meta
-                          (fn runner [& args]
-                            (run-hooks runner original args))
-                          (assoc (meta original)
-                            ::hooks (atom ()) ::original original))))))
+(defn- make-hook [key function]
+  {:key key :fn function})
 
-(defn add-hook
-  "Add a hook function f to target-var. Hook functions are passed the
-  target function and all their arguments and must apply the target to
-  the args if they wish to continue execution."
-  [target-var f]
-  (prepare-for-hooks target-var)
-  (swap! (::hooks (meta @target-var)) conj f))
+(defn- get-hooks [target qualifier]
+  ((::hooks (meta target)) qualifier))
 
-(defn remove-hook
-  "Remove hook function f from target-var."
-  [target-var f]
-  (when (::hooks (meta @target-var))
-    (swap! (::hooks (meta @target-var))
-           (partial remove #{f}))
-    (when (empty? @(::hooks (meta @target-var)))
-      (alter-var-root target-var
-                      (constantly (::original (meta @target-var)))))))
+(defn- get-hook [target qualifier key]
+  (when-let [hooks (get-hooks target qualifier)]
+    (some #(when (= key (:key %)) %) hooks)))
+
+(defn- prepared-for-hooks? [target]
+  (instance? clojure.lang.Ref (::hooks (meta target))))
+
+(defn- prepare-for-hooks [original]
+  (let [hooks (ref {:original original
+                    :before   ()
+                    :after    ()
+                    :around   ()})
+        main-block (fn [& args]
+                     (doseq [hook (reverse (:before @hooks))]
+                       (apply (:fn hook) args))
+                     (with-return (apply original args)
+                       (doseq [hook (:after @hooks)]
+                         (apply (:fn hook) args))))
+        around-block (join-hooks main-block
+                                 (reverse (map :fn (:around @hooks))))]
+    (with-meta (fn [& args] (apply around-block args))
+      (assoc (meta original) ::hooks hooks))))
+
+(defmulti remove-hook
+  (fn ([qualifier target key]
+        (cond (fn? target) ::fn
+              (var? target) ::var))))
+
+(defmulti add-hook
+  (fn ([qualifier target key hook]
+        (cond (fn? target) ::fn
+              (var? target) ::var))))
+
+(defmethod remove-hook ::fn
+  [qualifier target key]
+  (when (prepared-for-hooks? target)
+    (let [hooks-ref (::hooks (meta target))
+          hooks (qualifier @hooks-ref)]
+      (with-return target
+        (dosync (alter hooks-ref assoc qualifier
+                       (remove #(= key (:key %)) hooks)))))))
+
+(defmethod add-hook ::fn
+  [qualifier target key hook]
+  (let [target (if (prepared-for-hooks? target) target
+                   (prepare-for-hooks target))]
+    (remove-hook qualifier target key)
+    (let [hooks-ref (::hooks (meta target))
+          hooks (qualifier @hooks-ref)]
+      (with-return target
+        (dosync (alter hooks-ref assoc qualifier
+                       (conj hooks (make-hook key hook))))))))
+
+(defmethod remove-hook ::var
+  [qualifier target key]
+  (when (prepared-for-hooks? @target)
+    (remove-hook qualifier @target key)
+    ;; (when (empty? @(::hooks (meta @target-var)))
+    ;;   (alter-var-root target-var
+    ;;                   (constantly (::original (meta @target-var)))))
+    ))
+
+(defmethod add-hook ::var
+  [qualifier target key hook]
+  (when-not (prepared-for-hooks? @target)
+    (alter-var-root target prepare-for-hooks))
+  (add-hook qualifier @target key hook))
