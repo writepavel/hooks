9ed4e1689479fda6b35fe488437537b3fa13ea7c
Roman Zaharov <zahardzhan@gmail.com>
2010-06-21 12:27:50 +1100

    Change deps and rm robert dir.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject robert/hooke "1.0.1"
-  :description "Hooke your functions!"
-  :dev-dependencies [[org.clojure/clojure "1.1.0"]
+(defproject hooks "1.0.0"
+  :description "Hooks for functions and multimethods."
+  :dependencies     [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                     [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]])
